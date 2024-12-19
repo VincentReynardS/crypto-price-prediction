7bc36df6bf4c1c0d1dde190ddba05a4f6fad8c48
Vincent Reynard Satyadharma <vincent_reynard@hotmail.com>
2024-12-19 20:44:49 +1100

    Add function to export fine tuned model - WIP

--- services/news-signal/fine_tuning.py
+++ services/news-signal/fine_tuning.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional, Tuple
+from typing import Literal, Optional, Tuple
 
 import comet_ml
 from datasets import Dataset, load_dataset
@@ -7,6 +7,20 @@ from loguru import logger
 from transformers import AutoTokenizer, TrainingArguments
 from trl import SFTTrainer
 from unsloth import FastLanguageModel, is_bfloat16_supported
+
+# chat template we use to format the data we feed to the model
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
+
+instruction = '\nYou are an expert crypto financial analyst with deep knowledge of market dynamics and sentiment analysis.\nAnalyze the following news story and determine its potential impact on crypto asset prices.\nFocus on both direct mentions and indirect implications for each asset.\n\nDo not output data for a given coin if the news is not relevant to it.\n\n## Example input news story\n"Goldman Sachs wants to invest in Bitcoin and Ethereum, but not in XRP"\n\n## Example output\n[\n    {"coin": "BTC", "signal": 1},\n    {"coin": "ETH", "signal": 1},\n    {"coin": "XRP", "signal": -1},\n]\n'
 
 
 def load_base_llm_and_tokenizer(
@@ -136,6 +150,7 @@ def fine_tune(
     train_dataset: Dataset,
     test_dataset: Dataset,
     max_seq_length: int,
+    max_steps: int,
 ):
     """
     Fine-tunes the model using supervised fine tuning.
@@ -166,6 +181,7 @@ def fine_tune(
             seed=3407,
             output_dir='outputs',
             report_to='comet_ml',  # Use this for WandB etc
+            eval_strategy='epoch',
         ),
     )
 
@@ -173,12 +189,79 @@ def fine_tune(
     trainer.train()
 
 
+def sanity_check(
+    model: FastLanguageModel,
+    tokenizer: AutoTokenizer,
+    input_example: str,
+):
+    """
+    Just checking if the trained model works on a simple example
+    """
+    FastLanguageModel.for_inference(model)  # Enable native 2x faster inference
+    inputs = tokenizer(
+        [
+            alpaca_prompt.format(
+                instruction,  # instruction
+                input_example,  # input
+                '',  # output - leave this blank for generation!
+            )
+        ],
+        return_tensors='pt',
+    ).to('cuda')
+
+    outputs = model.generate(**inputs, max_new_tokens=128, use_cache=True)
+    output = tokenizer.batch_decode(outputs)
+    logger.info('Inference: {}', output)
+
+
+def export_model_to_ollama_format(
+    model: FastLanguageModel,
+    tokenizer: AutoTokenizer,
+    quantization_method: Optional[Literal['q4_k_m', 'f16', 'q8_0']] = 'q8_0',
+    # Optionally you can push it to HugginFace model registry
+    hf_username: Optional[str] = None,
+    hf_token: Optional[str] = None,
+):
+    """
+    Saves the model and the tokenizer to disk locally and pushes them
+    to the HF model registry
+
+    Args:
+        model:
+        tokenizer:
+        quantization_method:
+
+        hf_username
+        hf_token
+    """
+    # export the quantized model
+    logger.info('Saving model locally to disk')
+    model.save_pretrained_gguf(
+        'model', tokenizer, quantization_method=quantization_method
+    )
+    logger.info('Model saved to disk!')
+
+    # export the Ollama Modelfile
+
+    breakpoint()
+
+    # TODO: if you want to push to HF you need to get your HF username and generate
+    # a token at https://huggingface.co/settings/tokens
+    # model.push_to_hub_gguf(
+    #     "hf/model", # Change hf to your username!
+    #     tokenizer,
+    #     quantization_method = ["q4_k_m", "q8_0", "q5_k_m",],
+    #     token = "", # Get a token at https://huggingface.co/settings/tokens
+    # )
+
+
 def run(
     base_llm_name: str,
     dataset_path: str,
-    # comet_ml_api_key: str,
-    comet_ml_project_name: str,
     max_seq_length: Optional[int] = 2048,
+    max_steps: Optional[int] = 100,
+    comet_ml_api_key: Optional[str] = None,
+    comet_ml_project_name: Optional[str] = None,
 ):
     """
     Fine-tunes a base LLM using supervised fine tuning.
@@ -189,6 +272,13 @@ def run(
     Args:
         base_llm_name: The name of the base LLM to fine-tune
         dataset_path: The path to the dataset to use for fine-tuning
+        max_seq_length: The maximum sequence length to use
+        max_steps: The maximum number of steps to train for
+            We set it to a small number by default to debug faster.
+            Once we know the fine tuning is working, we can set it to a larger number.
+
+        comet_ml_api_key: The API key to use for CometML
+        comet_ml_project_name: The name of the CometML project to use
     """
     # 0. Login to CometML so we log training run metrics that we can see on CometML dashboard
     os.environ['COMET_LOG_ASSETS'] = 'True'
@@ -216,8 +306,23 @@ def run(
 
     # 4. Fine-tune the base LLM
     fine_tune(
-        model, tokenizer, train_dataset, test_dataset, max_seq_length=max_seq_length
+        model,
+        tokenizer,
+        train_dataset,
+        test_dataset,
+        max_seq_length=max_seq_length,
+        max_steps=max_steps,
     )
+
+    # 5. Inference on a few examples - sanity check
+    sanity_check(
+        model,
+        tokenizer,
+        input_example='Goldman Sachs considers doubling exposure on BTC and ETH. Remains skeptical about XRP',
+    )
+
+    # 6. Save model
+    export_model_to_ollama_format(model, tokenizer, quantization_method='q8_0')
 
 
 if __name__ == '__main__':
